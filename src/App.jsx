7d76e3dafdb9845c7f1c3deb1ee4c9ed859dbcb8
Anurag Bansal <devanuragbansal@gmail.com>
2026-01-06 14:56:14 +0530

    Refactor code structure for improved readability and maintainability

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import React, { useEffect, useRef, useState } from "react";
 import generateColor from "./utils/generateColor";
+import Confetti from "react-confetti";
 
 const letters = "abcdefghijklmnopqrstuvwxyz".split("");
 
 function App() {
   const [suggestedLetter, setSuggestedLetter] = useState(0);
+  const [isGameWon, setIsGameWon] = useState(false);
+  const [color, setColor] = useState(generateColor());
+
   const isPlayingRef = useRef(false);
 
   useEffect(() => {
     const handleKeyDown = (event) => {
-      if (isPlayingRef.current) return;
+      if (isPlayingRef.current || isGameWon) return;
 
       const key = event.key.toLowerCase();
       if (key.length !== 1 || !/[a-z]/.test(key)) return;
@@ -27,7 +31,18 @@ function App() {
 
       audio.onended = () => {
         isPlayingRef.current = false;
-        setSuggestedLetter((prev) => (prev + 1) % letters.length);
+        if (suggestedLetter === letters.length - 1) {
+          const audioWin = new Audio(`public/sounds/crowd-cheering.mp3`);
+          audioWin.play().catch((error) => {
+            console.error(`Error playing winning sound:`, error);
+          });
+
+          setIsGameWon(true);
+          return;
+        }
+
+        setSuggestedLetter((prev) => prev + 1);
+        setColor(generateColor());
       };
 
       audio.onerror = () => {
@@ -37,15 +52,19 @@ function App() {
 
     window.addEventListener("keydown", handleKeyDown);
     return () => window.removeEventListener("keydown", handleKeyDown);
-  }, [suggestedLetter]);
+  }, [suggestedLetter, isGameWon]);
 
   return (
     <main className="relative h-screen w-full bg-[#222222] text-white flex justify-center items-center bg-[url('/public/kids-bg.jpg')] bg-cover">
+      {isGameWon && (
+        <Confetti width={window.innerWidth} height={window.innerHeight} />
+      )}
+
       <div className="w-full h-full bg-black/20 backdrop-blur-sm flex justify-center items-center">
         <span
           className="text-9xl uppercase font-bold"
           style={{
-            color: `${generateColor()}`,
+            color,
           }}
         >
           {letters[suggestedLetter]}
